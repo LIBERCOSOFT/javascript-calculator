31d35704ab824fff14f076b553b5061107ce1013
OLADIPO-K <akinspreciousola@gmail.com>
2020-01-08 12:59:22 +0100

    Updates

---
+++ precache-manifest.96a09e5af7666d5814063e747b247d7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6164d12f474b0575e0a89e658d531ec",
+    "url": "/javascript-calculator/index.html"
+  },
+  {
+    "revision": "1f502efeb8190ee29f07",
+    "url": "/javascript-calculator/static/css/main.bc633b13.chunk.css"
+  },
+  {
+    "revision": "2581afbfdd070aba00da",
+    "url": "/javascript-calculator/static/js/2.d9ca92fb.chunk.js"
+  },
+  {
+    "revision": "1f502efeb8190ee29f07",
+    "url": "/javascript-calculator/static/js/main.bcf58797.chunk.js"
+  },
+  {
+    "revision": "af3dc86e0aee640c7c09",
+    "url": "/javascript-calculator/static/js/runtime~main.b6bacda3.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/javascript-calculator/static/media/logo.ee7cd8ed.svg"
+  }
+]);
